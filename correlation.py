c3531bbda26b4f83e7e5ed022d24aa42cf861f3e
Jaseem001 <jaseemseems@gmail.com>
2021-07-08 15:00:53 +0530

    changed file

--- correlation.py
+++ correlation.py
@@ -8,7 +8,8 @@ def load_journal(file_name):
     f.close()
     return datas
 
-def compute_phi(datas, event):
+def compute_phi(file_name, event):
+    datas = load_journal(file_name)
     n11 = n00 = n10 = n01 = n1p = n0p = np1 = np0 = 0
     for data in datas:
         if event in data['events']:
@@ -37,10 +38,14 @@ def compute_correlations(file_name):
             if event not in map.keys():
                 map[event] = 0
     for event in map.keys():
-        map[event] = compute_phi(datas, event)
+        map[event] = compute_phi(file_name, event)
     return map
 
 def diagnose(file_name):
     map = compute_correlations(file_name)
     map =  list(sorted(map.items(), key=lambda item: item[1]))
     return (map[len(map)-1][0], map[0][0])
+
+
+if __name__ == '__main__':
+    print(diagnose('journal.json'))
